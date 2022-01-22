8bcc9e16f77c3d9034563c800fafe0656445e3ac
aksamitksharma <aksamitksharma@gmail.com>
2022-01-22 22:00:49 +0530

    implemented useReducer with complex state & action

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # react-reducer or useReducer Hook
 
 **It is a hook that is used for managing states in React**
+
 **It is related to reducer functions**
+
 `useReducer(reducer, initialState)`
+
 `reducer(currentState, action)`
 
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import CounterOne from './componenets/CounterOne';
+import CounterTwo from './componenets/CounterTwo';
 
 function App() {
   return (
     <div className="App">
       <CounterOne />
+      <hr />
+      <CounterTwo />
     </div>
   );
 }
